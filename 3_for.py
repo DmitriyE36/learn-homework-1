05057b3e3074cf04c8269f48b3f5b0944060ace7
Dmitriy Elfimov <elf-dn@yandex.ru>
2022-06-12 00:21:37 +0300

    corrected the comments

--- 3_for.py
+++ 3_for.py
@@ -20,15 +20,12 @@ phones = [
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
-
+sum_sold_allpr = 0
 for phone in phones:
     sum_sold_pr = sum(phone['items_sold'])
     print(f'Суммарное количество продаж {phone["product"]}: {sum_sold_pr}')
-    avg_sold_pr = round(sum(phone['items_sold'])/len(phone['items_sold']))
+    avg_sold_pr = round(sum(phone['items_sold'])/len(phone['items_sold']), 2)
     print(f'Cреднее количество продаж {phone["product"]}: {avg_sold_pr}')
-
-sum_sold_allpr = 0
-for phone in phones:
     sum_sold_allpr += sum_sold_pr
 print(f'Суммарное количество продаж всех товаров: {sum_sold_allpr}')
 avg_sold_allpr = sum_sold_allpr/len(phones)
